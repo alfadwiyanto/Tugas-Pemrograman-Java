94e04f0fa228ec15e0933ca11cf1fcc51f331ce6
Alfa Dwiyanto <Jfaqumalfa@gmail.com>
2016-11-03 05:03:54 +0700

    12141368 awal program membuat segitiga dengan scanner, deklarasi variable x dan y

---
+++ src/Sikubawah.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Sikubawah {
+     public static void main(String[] args) {
+           Scanner input = new Scanner(System.in);
+      int x, y;
+      int tinggi;
+       System.out.printf("Tinggi Segitiga : ");
+    
+    }
+}
