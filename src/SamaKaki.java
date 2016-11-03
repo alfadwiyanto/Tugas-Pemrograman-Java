cbbcaf3d9e86e3629cbc0115fd39fbef34e91285
Alfa Dwiyanto <Jfaqumalfa@gmail.com>
2016-11-04 05:36:32 +0700

    12141368 segitiga sama kaki

--- src/SamaKaki.java
+++ src/SamaKaki.java
@@ -5,6 +5,19 @@ class SamaKaki {
         Scanner input = new Scanner(System.in);
         int a, b;
         int tinggi;
+        System.out.printf("Tinggi Segitiga : ");
+       tinggi = input.nextInt();
+       System.out.println("=========================");
+       
+       for(a=0; a<=tinggi; a++){
+           for(int d=1; d<=tinggi-a; d++){
+               System.out.print(" ");
+           }
+           for(int d=1; d<2*a; d++){
+           System.out.print("*");
+       }
+           System.out.println(" ");
     }
    
+}
 }
