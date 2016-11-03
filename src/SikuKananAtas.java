113a7f0175663949b9c30199f4b8ad18065a33b8
Alfa Dwiyanto <Jfaqumalfa@gmail.com>
2016-11-04 05:16:15 +0700

    12141368

--- src/SikuKananAtas.java
+++ src/SikuKananAtas.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 class SikuKananAtas {
     public static void main(String[] args) {
-        
+         Scanner input = new Scanner(System.in);
+        int x, y, z ;
+        int tinggi;
     }
 }
