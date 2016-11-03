5e497f4506f94d6fbe26b0a344c4ff5cbfe90908
Alfa Dwiyanto <Jfaqumalfa@gmail.com>
2016-11-04 05:31:24 +0700

    12141368 segitiga sama kaki

---
+++ src/SamaKaki.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class SamaKaki {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a, b;
+        int tinggi;
+    }
+   
+}
