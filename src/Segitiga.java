39447da080fe6af8e75d4bbd2f1f7e00bf9284e3
Alfa Dwiyanto <Jfaqumalfa@gmail.com>
2016-11-04 04:52:46 +0700

    12141368

---
+++ src/Segitiga.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Segitiga {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int x, y;
+      int tinggi;
+       System.out.printf("Tinggi Segitiga : ");
+       tinggi = input.nextInt();
+       System.out.println("=========================");
+    }
+    
+}
